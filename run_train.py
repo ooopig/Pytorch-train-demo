580378831abd0061bd5ba4e584fb12b3c9d44771
ooopig <1302972442@qq.com>
2022-04-26 17:15:25 +0800

    调整结构

--- run_train.py
+++ run_train.py
@@ -152,6 +152,62 @@ def train_step(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data) * args.world_size, len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
+
+def train(args, model, optimizer, lr_scheduler=None):
+    if args.rank == 0: args.logger.info('******train******')
+    best_acc = get_best_acc(args)
+    acc_deacy = 0
+
+    # 加载checkpoint
+    if args.begin_epoch > 1:
+        latest_model_dict_path = os.path.join(args.checkpoint_path, args.dataset,
+                                              '{}-epoch-{}.pkl'.format(args.model_type, args.begin_epoch - 1))
+        if os.path.exists(latest_model_dict_path):
+            checkpoint = load_checkpoint(args, latest_model_dict_path)
+            model.load_state_dict(checkpoint['model_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    else:
+        # 保存第一块GPU参数，其他GPU载入相同参数，必须保证所有参与训练的GPU参数是完全一致的
+        init_model_dict_path = os.path.join(args.checkpoint_path, args.dataset)
+        if rank == 0:
+            save_checkpoint(args, model, optimizer, init_model_dict_path)
+        dist.barrier()  # 用dist.barrier()来同步不同进程间的快慢
+        init_checkpoint = load_checkpoint(args,
+                                          os.path.join(init_model_dict_path, '{}-epoch-0.pkl'.format(args.model_type)))
+        model.load_state_dict(init_checkpoint['model_state_dict'])
+
+    # 训练
+    for epoch in range(args.begin_epoch, args.epochs + 1):
+        if rank == 0:
+            begin_time = time.time()
+        train_loader, val_loader = split_train_dataset(epoch, args.batch_size, args.ratio)
+        train_step(args, model, DEVICE, train_loader, optimizer, epoch)
+        acc, loss = inference(args, model, DEVICE, val_loader)
+
+        if rank == 0:
+            save_path = os.path.join(args.checkpoint_path, args.dataset)
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+            if best_acc < acc:
+                acc_deacy = 0
+                save_checkpoint(args, model, optimizer, save_path, epoch, acc, loss, is_best=True)
+            else:
+                acc += 1
+                # early stoping
+                if acc == args.early_stop_num:
+                    args.logger.info('early stopping')
+                    break
+                save_checkpoint(args, model, optimizer, save_path, epoch, acc, loss, is_best=False)
+            end_time = time.time()
+            args.logger.info('Epoch:{} run time:{}s lr:{} acc:{} loss:{}'.format(epoch, int(end_time - begin_time),
+                                                                                 optimizer.param_groups[0]['lr']
+                                                                                 , acc, loss))
+        # 动态改变学习率
+        if args.learning_scheduler:
+            lr_scheduler.step()
+
+    cleanup()
+
 def inference(args, model, device, test_loader):
     model.eval()
     test_loss = 0.0
@@ -203,14 +259,6 @@ if __name__ == '__main__':
     model = Net(args)
     model = model.to(DEVICE)
     args.logger.info('model load to-{}'.format(DEVICE))
-    args.learning_rate *= sqrt(args.world_size) # 学习率根据并行GPU数目倍增,n为GPU数目，lr变为n的平方根倍
-    optimizer = optim.Adam([{'params': model.parameters(), 'initial_lr': args.learning_rate}],lr=args.learning_rate)
-
-    lr_scheduler = WarmupCosineLR(optimizer=optimizer, lr_min=1e-6, lr_max=args.learning_rate, warmup_iters=args.warmup_iters,
-                                  T_max=args.epochs,warmup_factor= args.warmup_factor,
-                                  last_epoch=args.begin_epoch-1)
-    # lr_scheduler = WarmupMultiStepLR(optimizer=optimizer,milestones=args.milestones,gamma=args.decay_ratio,last_epoch=args.begin_epoch-1,
-    #                                 warmup_factor=args.warmup_factor, warmup_iters=args.warmup_iters)
 
     if args.syncBN:
         model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model).to(DEVICE)
@@ -222,68 +270,29 @@ if __name__ == '__main__':
         print_all_parameters(args)
 
     if args.train:
-        if rank == 0: args.logger.info('******train******')
-        best_acc = get_best_acc(args)
-        acc_deacy = 0
+        args.learning_rate *= sqrt(args.world_size)  # 学习率根据并行GPU数目倍增,n为GPU数目，lr变为n的平方根倍
+        optimizer = optim.Adam([{'params': model.parameters(), 'initial_lr': args.learning_rate}],
+                               lr=args.learning_rate)
 
-        # 加载checkpoint
-        if args.begin_epoch > 1:
-            latest_model_dict_path = os.path.join(args.checkpoint_path, args.dataset,
-                                                  '{}-epoch-{}.pkl'.format(args.model_type, args.begin_epoch - 1))
-            if os.path.exists(latest_model_dict_path):
-                checkpoint = load_checkpoint(args, latest_model_dict_path)
-                model.load_state_dict(checkpoint['model_state_dict'])
-                optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        else:
-            # 保存第一块GPU参数，其他GPU载入相同参数，必须保证所有参与训练的GPU参数是完全一致的
-            init_model_dict_path = os.path.join(args.checkpoint_path, args.dataset)
-            if rank == 0:
-                save_checkpoint(args,model,optimizer,init_model_dict_path)
-            dist.barrier() # 用dist.barrier()来同步不同进程间的快慢
-            init_checkpoint = load_checkpoint(args, os.path.join(init_model_dict_path, '{}-epoch-0.pkl'.format(args.model_type)))
-            model.load_state_dict(init_checkpoint['model_state_dict'])
+        lr_scheduler = WarmupCosineLR(optimizer=optimizer, lr_min=1e-6, lr_max=args.learning_rate,
+                                      warmup_iters=args.warmup_iters,
+                                      T_max=args.epochs, warmup_factor=args.warmup_factor,
+                                      last_epoch=args.begin_epoch - 1)
+        # lr_scheduler = WarmupMultiStepLR(optimizer=optimizer,milestones=args.milestones,gamma=args.decay_ratio,last_epoch=args.begin_epoch-1,
+        #                                 warmup_factor=args.warmup_factor, warmup_iters=args.warmup_iters)
 
-        # 训练
-        for epoch in range(args.begin_epoch, args.epochs+1):
-            if rank == 0:
-                begin_time = time.time()
-            train_loader, val_loader = split_train_dataset(epoch,args.batch_size, args.ratio)
-            train_step(args,model, DEVICE, train_loader, optimizer, epoch)
-            acc, loss = inference(args,model, DEVICE, val_loader)
-
-            if rank == 0:
-                save_path = os.path.join(args.checkpoint_path,args.dataset)
-                if not os.path.exists(save_path):
-                    os.mkdir(save_path)
-                if best_acc < acc:
-                    acc_deacy = 0
-                    save_checkpoint(args, model, optimizer, save_path, epoch, acc, loss, is_best=True)
-                else:
-                    acc += 1
-                    # early stoping
-                    if acc == args.early_stop_num:
-                        args.logger.info('early stopping')
-                        break
-                    save_checkpoint(args, model, optimizer, save_path, epoch, acc, loss, is_best=False)
-                end_time = time.time()
-                args.logger.info('Epoch:{} run time:{}s lr:{} acc:{} loss:{}'.format(epoch, int(end_time-begin_time),
-                                                                                     optimizer.param_groups[0]['lr']
-                                                                                     ,acc,loss))
-            # 动态改变学习率
-            if args.learning_scheduler:
-                lr_scheduler.step()
-
-        cleanup()
+        train(args, model, optimizer, lr_scheduler)
     elif args.inference:
-        best_model_dict_path = os.path.join(args.checkpoint_path,args.dataset, '{}-model_best_dict.pkl'.format(args.model_type))
-        best_model_path = os.path.join(args.checkpoint_path,args.dataset, '{}-model_best.pkl'.format(args.model_type))
+        best_model_dict_path = os.path.join(args.checkpoint_path, args.dataset,
+                                            '{}-model_best_dict.pkl'.format(args.model_type))
+        best_model_path = os.path.join(args.checkpoint_path, args.dataset, '{}-model_best.pkl'.format(args.model_type))
         if args.save_whole_modle:
             model = torch.load(best_model_path)
         else:
             checkpoint = load_checkpoint(args, best_model_dict_path)
             model.load_state_dict(checkpoint['model_state_dict'])
         test_loader = split_test_dataset(args.batch_size)
-        acc, loss = inference(args,model, DEVICE, test_loader)
+        acc, loss = inference(args, model, DEVICE, test_loader)
     else:
         if rank == 0:
             args.logger.info('请选择正确的模式(train/inference)')
