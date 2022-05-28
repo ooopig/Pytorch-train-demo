2650df840f56486cfa169bb4911975f7455c107f
ooopig <1302972442@qq.com>
2022-05-28 19:41:27 +0800

    lr

--- src/lr.py
+++ src/lr.py
@@ -8,6 +8,7 @@
 @description: 
 """
 from bisect import bisect_right
+from math import cos, pi
 
 from torch.optim import lr_scheduler
 import numpy as np
@@ -116,3 +117,15 @@ class WarmupMultiStepLR(lr_scheduler._LRScheduler):
         return [base_lr * warmup_factor * self.gamma ** bisect_right(self.milestones, self.last_epoch) for base_lr in
                 self.base_lrs]  # self.base_lrs,optimizer初始学习率weight_lr=0.0003，bias_lr=0.0006
 
+def adjust_learning_rate(optimizer, current_epoch, max_epoch, lr_min=0, lr_max=0.1, warmup=True):
+    warmup_epoch = 10 if warmup else 0
+    if current_epoch < warmup_epoch:
+        lr = lr_max * current_epoch / warmup_epoch
+    elif current_epoch < max_epoch:
+        lr = lr_min + (lr_max - lr_min) * (
+                    1 + cos(pi * (current_epoch - warmup_epoch) / (max_epoch - warmup_epoch))) / 2
+    else:
+        lr = lr_min + (lr_max - lr_min) * (
+                1 + cos(pi * (current_epoch-max_epoch) / (max_epoch))) / 2
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
